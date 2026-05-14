d017e0e376d5383c9c1dc2df9ae406f3ce61c0b1
Dun <dun4ik12@Gmail.com>
2026-05-14 19:56:33 +0300

    add suppors of chess.com

--- src/lichgame.ts
+++ src/lichgame.ts
@@ -6,11 +6,11 @@ export interface LichessGame {
         white: { user: { name: string }; rating: number; ratingDiff: number };
         black: { user: { name: string }; rating: number; ratingDiff: number };
     };
-    createdAt: number;
+    lastMoveAt: number;
     winner?: string;
 }
 
-export default async function import_games(nick: string, since_t: number, until_t: number): Promise<LichessGame[]> {
+export default async function import_games_lichess(nick: string, since_t: number, until_t: number): Promise<LichessGame[]> {
     const res = await requestUrl({
         url: `https://lichess.org/api/games/user/${nick}?since=${since_t}&until=${until_t}`,
         headers: {
--- src/obrab_games.ts
+++ src/obrab_games.ts
@@ -1,4 +1,5 @@
 import { LichessGame } from "./lichgame.js";
+import { ChesscomGame } from "./chesscomgames.js";
 
 export interface GameStat {
     wins: number;
@@ -14,23 +15,101 @@ export interface GameStat {
     percentage_of_games_for_black: number;
     player_colors: Array<"white" | "black">;
 }
+//chesscom
 
-export function obrab_id(games: LichessGame[]): string[] {
+export function obrab_id_chesscom(games: ChesscomGame[]): string[] {
+    return games.map((game) => game.uuid);
+}
+
+export function obrab_date_chesscom(games: ChesscomGame[]) :string[] {
+    return games.map((game) => new Date(game.end_time).toLocaleString());
+}
+
+
+export function calcStat_chesscom(nick: string, games: ChesscomGame[]): GameStat {
+    let wins = 0;
+    let defeats = 0;
+    let draws = 0;
+    const player_colors: Array<"white" | "black"> = [];
+    const total = games.length;
+
+    games.forEach((game) => {
+        const color: "white" | "black" =
+            game.white.username === nick ? "white" : "black";
+        player_colors.push(color);
+
+        let result:string;
+        if (color === "white") {
+            result = game.white.result;
+        }
+        else {
+            result = game.black.result;
+        }
+
+        const win = result === "win";
+
+        const loss = [
+            "checkmated",
+            "timeout",
+            "resigned",
+            "lose"
+        ].includes(result);
+
+        const draw = [
+            "agreed",
+            "repetition",
+            "stalemate",
+            "insufficient",
+            "timevsinsufficient",
+            "50move"
+        ].includes(result);
+
+        if (win) wins++;
+        if (loss) defeats++;
+        if (draw) draws++;
+    });
+
+    const number_of_games_for_white = player_colors.filter(
+        (c) => c === "white"
+    ).length;
+    const number_of_games_for_black = total - number_of_games_for_white;
+    return {
+        wins,
+        defeats,
+        draws,
+        winning_percentage: total ? wins / total : 0,
+        percentage_of_defeats: total ? defeats / total : 0,
+        percentage_of_draws: total ? draws / total : 0,
+        number_of_games_played: total,
+        number_of_games_for_white,
+        number_of_games_for_black,
+        percentage_of_games_for_white: total
+            ? number_of_games_for_white / total
+            : 0,
+        percentage_of_games_for_black: total
+            ? number_of_games_for_black / total
+            : 0,
+        player_colors,
+    };
+}
+//lichess
+
+export function obrab_id_lichess(games: LichessGame[]): string[] {
     return games.map((game) => game.id);
 }
 
-export function obrab_date(games: LichessGame[]): string[] {
-    return games.map((game) => new Date(game.createdAt).toLocaleString());
+export function obrab_date_lichess(games: LichessGame[]): string[] {
+    return games.map((game) => new Date(game.lastMoveAt).toLocaleString());
 }
 
-export function obrab_ratingDiff(nick: string, games: LichessGame[]): number[] {
+export function obrab_ratingDiff_lichess(nick: string, games: LichessGame[]): number[] {
     return games.map((game) => {
         const color = game.players.white.user.name === nick ? "white" : "black";
         return game.players[color].ratingDiff;
     });
 }
 
-export function calcStat(nick: string, games: LichessGame[]): GameStat {
+export function calcStat_lichess(nick: string, games: LichessGame[]): GameStat {
     let wins = 0;
     let defeats = 0;
     let draws = 0;
